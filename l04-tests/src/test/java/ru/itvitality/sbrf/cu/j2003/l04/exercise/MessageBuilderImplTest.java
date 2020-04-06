677af7eb1b49605d215627e7bbce0862bee200af
kataus <kataus@gmail.com>
2020-04-06 18:43:07 +0300

    Добавлен пример по generics для лекции  02.04.2020

---
+++ l04-tests/src/test/java/ru/itvitality/sbrf/cu/j2003/l04/exercise/MessageBuilderImplTest.java
@@ -0,0 +1,17 @@
+package ru.itvitality.sbrf.cu.j2003.l04.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBuilderImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void buildMessage() {
+    }
+}
