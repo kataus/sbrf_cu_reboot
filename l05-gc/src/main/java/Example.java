530dbbb8bb07e585abc4a32c8b68e8d70b77ebbb
kataus <kataus@gmail.com>
2020-04-09 18:22:22 +0300

    Добавлен пример по порядку оформления класса

---
+++ l05-gc/src/main/java/Example.java
@@ -0,0 +1,23 @@
+public class Example {
+    public static final String CONST_1 = "123";
+
+    private int index;
+
+    public Example(){
+
+    }
+
+    public void publicMethod(){
+
+    }
+
+    protected void protectedMethod(){
+
+    }
+
+    private void privateMethod(){
+
+    }
+
+    // internal classes
+}
